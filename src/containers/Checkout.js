aa3c99415d7b8881067ac5b2ddf1cf9a3d1ea0dc
ArtusU <artusrock@hotmail.com>
2021-09-21 14:50:50 +0100

    Checkout.js refactoring

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -25,12 +25,101 @@ import { authAxios } from "../utils";
 import { checkoutURL, orderSummaryURL, addCouponURL } from "../constants";
 
 
-class OrderPreview extends Component {
+const OrderPreview = props => {
+  const { data } = props;
+  return (
+    <React.Fragment>
+      { data && 
+        <React.Fragment>
+          <Item.Group relaxed>
+          {data.order_items.map((orderItem, i) => {
+            console.log(orderItem)
+            return (
+              <Item key={i}>
+                <Item.Image 
+                size='tiny' 
+                src={`http://127.0.0.1:8000${orderItem.item_obj.image}`}
+                />
+                <Item.Content verticalAlign='middle'>
+                  <Item.Header 
+                    as='a'
+                    >
+                    {orderItem.quantity} x {orderItem.item_obj.title}
+                  </Item.Header>
+                  <Item.Extra>
+                    <Label>
+                      £{orderItem.final_price}
+                    </Label>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+            )
+          })}
+          
+            
+          </Item.Group>
+
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Item.Header>
+                  Order Total: £{data.total}
+                </Item.Header>
+                {data.coupon && <Label color='yellow' style={{ marginLeft: '10px'}}>Current coupon: £{data.coupon.amount} OFF </Label>}
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </React.Fragment>
+      }
+      
+    </React.Fragment>
+  )
+};
+
+class CouponForm extends Component {
+  state = {
+    code: ''
+  };
+
+  handleChange = e => {
+    this.setState({
+      code: e.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    const { code } = this.state;
+    this.props.handleAddCoupon(e, code);
+    this.setState({code: ''});
+  };
+
+  render () {
+    const { code } = this.state;
+    return (
+      <React.Fragment>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Coupon code</label>
+            <input
+              placeholder="Enter a coupon.."
+              value={code}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </React.Fragment> 
+    );
+  }
+}
+
+class CheckoutForm extends Component {
   state = {
     data: null,
     loading: false,
-    error: null
-  };
+    error: null,
+    success: false
+  }
 
   componentDidMount() {
     this.handleFetchOrder();
@@ -53,163 +142,16 @@ class OrderPreview extends Component {
       });
   };
 
-  render() {
-    const {data, error, loading } = this.state;
-    return (
-      <React.Fragment>
-        {error && (
-          <Message
-            error
-            header="There was an error"
-            content={JSON.stringify(error)}
-          />
-        )}
-        {loading && (
-          <Segment>
-            <Dimmer active inverted>
-              <Loader inverted>Loading</Loader>
-            </Dimmer>
-
-            <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
-          </Segment>
-        )}
-        { data && 
-          <React.Fragment>
-            <Item.Group relaxed>
-            {data.order_items.map((orderItem, i) => {
-              console.log(orderItem)
-              return (
-                <Item key={i}>
-                  <Item.Image 
-                  size='tiny' 
-                  src={`http://127.0.0.1:8000${orderItem.item_obj.image}`}
-                  />
-                  <Item.Content verticalAlign='middle'>
-                    <Item.Header 
-                      as='a'
-                      >
-                      {orderItem.quantity} x {orderItem.item_obj.title}
-                    </Item.Header>
-                    <Item.Extra>
-                      <Label>
-                        £{orderItem.final_price}
-                      </Label>
-                    </Item.Extra>
-                  </Item.Content>
-                </Item>
-
-                // <Table.Row key={orderItem.id}>
-                //   <Table.Cell>
-                //     {i}
-                //   </Table.Cell>
-                //   <Table.Cell>{orderItem.item}</Table.Cell>
-                //   <Table.Cell>£{orderItem.item_obj.price}</Table.Cell>
-                //   <Table.Cell>{ orderItem.quantity }</Table.Cell>
-                //   <Table.Cell>
-                //     {orderItem.item.discount_price && (
-                //       <Label color="green" ribbon>
-                //         ON DISCOUNT
-                //       </Label>
-                //     )}
-                //     £{orderItem.final_price}
-                //     <Icon
-                //       name="trash"
-                //       color="red"
-                //       style={{ float: "right", cursor: "pointer" }}
-                //     />
-                //   </Table.Cell>
-                // </Table.Row>
-              )
-            })}
-            
-              
-            </Item.Group>
-
-            <Item.Group>
-              <Item>
-                <Item.Content>
-                  <Item.Header>
-                    Order Total: £{data.total}
-                  </Item.Header>
-                  {data.coupon && <Label color='yellow' style={{ marginLeft: '10px'}}>Current coupon: £{data.coupon.amount} OFF </Label>}
-                </Item.Content>
-              </Item>
-            </Item.Group>
-          </React.Fragment>
-        }
-        
-      </React.Fragment>
-    )
-  };
-};
-
-class CouponForm extends Component {
-  state = {
-    loading: false,
-    error: null,
-    code: ''
-  };
-
-  handleAddCoupon = e => {
+  handleAddCoupon = (e, code) => {
     e.preventDefault();
     this.setState({ loading: true })
-    const {code} = this.state;
     authAxios.post(addCouponURL, {code})
     .then(res => {
-      this.setState({loading: false})
+      this.setState({loading: false});
+      this.handleFetchOrder();
     }).catch(err => {
       this.setState({error: err, loading: false})
     })
-  }
-
-  handleChange = e => {
-    this.setState({
-      code: e.target.value
-    })
-  }
-  render () {
-    const { code, error, loading } = this.state;
-    return (
-      <React.Fragment>
-        {error && (
-          <Message
-            error
-            header="There was an error"
-            content={JSON.stringify(error)}
-          />
-        )}
-        {loading && (
-          <Segment>
-            <Dimmer active inverted>
-              <Loader inverted>Loading</Loader>
-            </Dimmer>
-
-            <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
-          </Segment>
-        )}
-      
-        <Form onSubmit={this.handleAddCoupon}>
-          <Form.Field>
-            <label>Coupon code</label>
-            <input
-              placeholder="Enter a coupon.."
-              value={code}
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Button type="submit">Submit</Button>
-        </Form>
-      </React.Fragment> 
-    );
-  }
-}
-
-class CheckoutForm extends Component {
-  state = {
-    data: null,
-    loading: false,
-    error: null,
-    success: false
   }
 
   submit = ev => {
@@ -240,31 +182,31 @@ class CheckoutForm extends Component {
 
 
   render() {
-    const { error, loading, success } = this.state;
+    const { data, error, loading } = this.state;
     
     return (
       <div>
-        { error && (
+        {error && (
           <Message
-          error
-          header="There was some errors with your submission"
-          content={JSON.stringify(error)} 
+            error
+            header="There was some errors with your submission"
+            content={JSON.stringify(error)}
           />
         )}
-        { success && (
-          <Message positive>
-            <Message.Header>Your payment was successful</Message.Header>
-            <p>
-              Go to your <b>profile</b> to see your order delivery status.
-            </p>
-          </Message>
+        {loading && (
+          <Segment>
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+            <Image src="/images/wireframe/short-paragraph.png" />
+          </Segment>
         )}
 
-        <OrderPreview />
+        <OrderPreview data={data} />
 
         <Divider />
 
-        <CouponForm />
+        <CouponForm handleAddCoupon={(e, code) => this.handleAddCoupon(e, code)} />
 
         <Divider />
 
