d56a5bb5f64c6cb39418d42810d3dc83e26d07be
Artus U <artusrock@hotmail.com>
2021-11-03 13:06:30 +0000

    refactoring Profile.js code

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router';
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router";
 import {
   Button,
   Card,
@@ -16,110 +16,99 @@ import {
   Message,
   Segment,
   Select,
-  Table
-} from 'semantic-ui-react';
-import { addressListURL, addressCreateURL, countryListURL, userIDURL } from '../constants';
-import { authAxios } from '../utils';
+  Table,
+} from "semantic-ui-react";
+import {
+  addressListURL,
+  addressCreateURL,
+  countryListURL,
+  userIDURL,
+} from "../constants";
+import { authAxios } from "../utils";
 
+const UPDATE_FORM = "UPDATE_FORM";
+const CREATE_FORM = "CREATE_FORM";
 
-// class AddressForm extends React.Component {
-
-//   render() {
-//     return (
-      
-//     );
-//   }
-// }
-
-class Profile extends React.Component {
-  state = { 
-    activeItem: 'billingAddress',
-    error: null,
-    loading: false,
-    addresses: [],
-    countries: [],
-    formData: { default: false },
-    saving: false,
-    success: false,
-    userID: null
-  }
+class PaymentHistory extends React.Component {
+  state = {
+    payments: [],
+  };
 
   componentDidMount() {
-    this.handleFetchAddresses();
-    this.handleFetchCountries();
-    this.handleFetchUserID();
+    this.handleFetchPayments();
   }
 
-  handleItemClick = name => {
-    this.setState({ activeItem: name }, () => {
-      this.handleFetchAddresses();
-    });
+  render() {
+    const { payments } = this.state;
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>Amount</Table.HeaderCell>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {payments.map((p) => {
+            return (
+              <Table.Row key={p.id}>
+                <Table.Cell>{p.id}</Table.Cell>
+                <Table.Cell>${p.amount}</Table.Cell>
+                <Table.Cell>{new Date(p.timestamp).toUTCString()}</Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    );
+  }
+}
+
+class AddressForm extends React.Component {
+  state = {
+    error: null,
+    loading: false,
+    formData: {
+      address_type: "",
+      apartment_address: "",
+      country: "",
+      default: false,
+      id: "",
+      street_address: "",
+      user: 1,
+      zip: "",
+    },
+    saving: false,
+    success: false,
   };
 
-  handleFormatCountries = countries => {
-    const keys = Object.keys(countries);
-    return keys.map(k => {
-      return {
-        key: k,
-        text: countries[k],
-        value: k
-      };
-    });
-  };
-
-  handleFetchUserID = () => {
-    authAxios
-      .get(userIDURL)
-      .then(res => {
-        console.log(res.data)
-        this.setState({ userID: res.data.userID });
-      })
-      .catch(err => {
-        this.setState({ error: err });
-      });
-  };
-
-  handleFetchCountries = () => {
-    authAxios.get(countryListURL)
-    .then(res => {
-      this.setState({ countries: this.handleFormatCountries(res.data) });
-    })
-    .catch(err => {
-      this.setState({error: err });
-    });
-  };
-
-  handleFetchAddresses = () => {
-    this.setState({ loading: true })
-    const { activeItem } = this.state;
-    authAxios.get(addressListURL(activeItem === "billingAddress" ? "B" : "S"))
-    .then(res => {
-      this.setState({ addresses: res.data, loading: false })
-    })
-    .catch(err => {
-      this.setState({error: err });
-    });
-  };
+  componentDidMount() {
+    const { address, formType } = this.props;
+    if (formType === UPDATE_FORM) {
+      this.setState({ formData: address });
+    }
+  }
 
   handleToggleDefault = () => {
     const { formData } = this.state;
     const updatedFormdata = {
       ...formData,
-      default: !formData.default
+      default: !formData.default,
     };
     this.setState({
-      formData: updatedFormdata
+      formData: updatedFormdata,
     });
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     const { formData } = this.state;
     const updatedFormdata = {
       ...formData,
-      [e.target.name]: e.target.value
-    }
+      [e.target.name]: e.target.value,
+    };
     this.setState({
-      formData: updatedFormdata
+      formData: updatedFormdata,
     });
   };
 
@@ -127,44 +116,269 @@ class Profile extends React.Component {
     const { formData } = this.state;
     const updatedFormdata = {
       ...formData,
-      [name]: value
+      [name]: value,
     };
     this.setState({
-      formData: updatedFormdata
+      formData: updatedFormdata,
     });
   };
 
-  handleCreateAddress = e => {
-    this.setState({ saving: true })
+  handleSubmit = (e) => {
+    this.setState({ saving: true });
     e.preventDefault();
-    const { activeItem, formData, userID } = this.state;
+    const { formType } = this.props;
+    if (formType === UPDATE_FORM) {
+      this.handleUpdateAddress();
+    } else {
+      this.handleCreateAddress();
+    }
+  };
+
+  handleCreateAddress = () => {
+    const { userID, activeItem } = this.props;
+    const { formData } = this.state;
     authAxios
       .post(addressCreateURL, {
         ...formData,
         user: userID,
-        address_type: activeItem === "billingAddress" ? "B" : "S"
+        address_type: activeItem === "billingAddress" ? "B" : "S",
       })
-      .then(res => {
+      .then((res) => {
         this.setState({
           saving: false,
           success: true,
+          formData: { default: false },
         });
         this.props.callback();
       })
-      .catch(err => {
+      .catch((err) => {
         this.setState({ error: err });
       });
   };
 
+  render() {
+    const { countries } = this.props;
+    const { error, formData, success, saving } = this.state;
+    return (
+      <Form onSubmit={this.handleSubmit} success={success} error={error}>
+        <Form.Input
+          required
+          name="street_address"
+          placeholder="Street address"
+          onChange={this.handleChange}
+          value={formData.street_address}
+        />
+        <Form.Input
+          required
+          name="apartment_address"
+          placeholder="Apartment address"
+          onChange={this.handleChange}
+          value={formData.apartment_address}
+        />
+        <Form.Field required>
+          <Select
+            loading={countries.length < 1}
+            fluid
+            clearable
+            search
+            options={countries}
+            name="country"
+            placeholder="Country"
+            onChange={this.handleSelectChange}
+            value={formData.country}
+          />
+        </Form.Field>
+        <Form.Input
+          required
+          name="zip"
+          placeholder="Zip code"
+          onChange={this.handleChange}
+          value={formData.zip}
+        />
+        <Form.Checkbox
+          name="default"
+          label="Make this the default address?"
+          onChange={this.handleToggleDefault}
+          checked={formData.default}
+        />
+        {success && (
+          <Message success header="Success!" content="Your address was saved" />
+        )}
+        {error && (
+          <Message
+            error
+            header="There was an error"
+            content={JSON.stringify(error)}
+          />
+        )}
+        <Form.Button disabled={saving} loading={saving} primary>
+          Save
+        </Form.Button>
+      </Form>
+    );
+  }
+}
+
+class Profile extends React.Component {
+  state = {
+    activeItem: "billingAddress",
+    addresses: [],
+    countries: [],
+    userID: null,
+    selectedAddress: null,
+  };
+
+  componentDidMount() {
+    this.handleFetchAddresses();
+    this.handleFetchCountries();
+    this.handleFetchUserID();
+  }
+
+  handleItemClick = (name) => {
+    this.setState({ activeItem: name }, () => {
+      this.handleFetchAddresses();
+    });
+  };
+
+  handleGetActiveItem = () => {
+    const { activeItem } = this.state;
+    if (activeItem === "billingAddress") {
+      return "Billing Address";
+    } else if (activeItem === "shippingAddress") {
+      return "Shipping Address";
+    }
+    return "Payment History";
+  };
+
+  handleFormatCountries = (countries) => {
+    const keys = Object.keys(countries);
+    return keys.map((k) => {
+      return {
+        key: k,
+        text: countries[k],
+        value: k,
+      };
+    });
+  };
+
+  handleSelectAddress = (address) => {
+    this.setState({ selectedAddress: address });
+  };
+
+  handleFetchUserID = () => {
+    authAxios
+      .get(userIDURL)
+      .then((res) => {
+        this.setState({ userID: res.data.userID });
+      })
+      .catch((err) => {
+        this.setState({ error: err });
+      });
+  };
+
+  handleFetchCountries = () => {
+    authAxios
+      .get(countryListURL)
+      .then((res) => {
+        this.setState({ countries: this.handleFormatCountries(res.data) });
+      })
+      .catch((err) => {
+        this.setState({ error: err });
+      });
+  };
+
+  handleFetchAddresses = () => {
+    this.setState({ loading: true });
+    const { activeItem } = this.state;
+    authAxios
+      .get(addressListURL(activeItem === "billingAddress" ? "B" : "S"))
+      .then((res) => {
+        this.setState({ addresses: res.data, loading: false });
+      })
+      .catch((err) => {
+        this.setState({ error: err });
+      });
+  };
+
+  handleCallback = () => {
+    this.handleFetchAddresses();
+    this.setState({ selectedAddress: null });
+  };
+
+  renderAddresses = () => {
+    const { activeItem, addresses, countries, selectedAddress, userID } =
+      this.state;
+    return (
+      <React.Fragment>
+        <Card.Group>
+          {addresses.map((a) => {
+            return (
+              <Card key={a.id}>
+                <Card.Content>
+                  {a.default && (
+                    <Label as="a" color="blue" ribbon="right">
+                      Default
+                    </Label>
+                  )}
+                  <Card.Header>
+                    {a.street_address}, {a.apartment_address}
+                  </Card.Header>
+                  <Card.Meta>{a.country}</Card.Meta>
+                  <Card.Description>{a.zip}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Button
+                    color="yellow"
+                    onClick={() => this.handleSelectAddress(a)}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    color="red"
+                    onClick={() => this.handleDeleteAddress(a.id)}
+                  >
+                    Delete
+                  </Button>
+                </Card.Content>
+              </Card>
+            );
+          })}
+        </Card.Group>
+        {addresses.length > 0 ? <Divider /> : null}
+        {selectedAddress === null ? (
+          <AddressForm
+            activeItem={activeItem}
+            countries={countries}
+            formType={CREATE_FORM}
+            userID={userID}
+            callback={this.handleCallback}
+          />
+        ) : null}
+        {selectedAddress && (
+          <AddressForm
+            activeItem={activeItem}
+            userID={userID}
+            countries={countries}
+            address={selectedAddress}
+            formType={UPDATE_FORM}
+            callback={this.handleCallback}
+          />
+        )}
+      </React.Fragment>
+    );
+  };
 
   render() {
-    const { activeItem, error, loading, addresses, countries, saving, success } = this.state;
-
+    const { activeItem, error, loading } = this.state;
+    const { isAuthenticated } = this.props;
+    if (!isAuthenticated) {
+      return <Redirect to="/login" />;
+    }
     return (
       <Grid container columns={2} divided>
         <Grid.Row columns={1}>
           <Grid.Column>
-          {error && (
+            {error && (
               <Message
                 error
                 header="There was an error"
@@ -179,8 +393,6 @@ class Profile extends React.Component {
                 <Image src="/images/wireframe/short-paragraph.png" />
               </Segment>
             )}
-            
-
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
@@ -203,100 +415,25 @@ class Profile extends React.Component {
               />
             </Menu>
           </Grid.Column>
-          
           <Grid.Column width={10}>
-            <Header>Header</Header>
+            <Header>{this.handleGetActiveItem()}</Header>
             <Divider />
-            <Card.Group>
-              {addresses.map(a => {
-                console.log(a)
-                return (
-                  <Card key={a.id}>
-                    <Card.Content>
-                      {a.default && (
-                        <Label as="a" color="blue" ribbon="right">
-                          Default
-                        </Label>
-                      )}
-                      <Card.Header>
-                        {a.street_address}, {a.apartment_address}
-                      </Card.Header>
-                      <Card.Description>{a.zip}</Card.Description>
-                      <Card.Description>{a.country}</Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                      <Button
-                        color="yellow"
-                        onClick={() => this.handleSelectAddress(a)}
-                      >
-                        Update
-                      </Button>
-                      <Button
-                        color="red"
-                        onClick={() => this.handleDeleteAddress(a.id)}
-                      >
-                        Delete
-                      </Button>
-                    </Card.Content>
-                  </Card>
-                );
-              })}
-            </Card.Group>
-            {addresses.length > 0 ? <Divider /> : null }
-            {
-              activeItem === 'billingAddress' ? (
-              <Form onSubmit={this.handleCreateAddress} success={success}>
-                <Form.Input
-                  required
-                  name="street_address"
-                  placeholder="Street address"
-                  onChange={this.handleChange}
-                />
-                <Form.Input
-                  required
-                  name="apartment_address"
-                  placeholder="Apartment address"
-                  onChange={this.handleChange}
-                />
-                <Form.Field required>
-                  <Select
-                    loading={countries.length < 1 }
-                    fluid
-                    clearable
-                    search
-                    options={countries}
-                    name="country"
-                    placeholder="Country"
-                    onChange={this.handleSelectChange}
-                  />
-                </Form.Field>
-                <Form.Input
-                  required
-                  name="zip"
-                  placeholder="Zip code"
-                  onChange={this.handleChange}
-                />
-                <Form.Checkbox
-                  name="default"
-                  label="Make this the default address?"
-                  onChange={this.handleToggleDefault}
-                />
-                {success && (
-                  <Message success header="Success!" content="Your address was saved" />
-                )}
-                <Form.Button disabled={saving} loading={saving} primary>
-                  Save
-                </Form.Button>
-              </Form>
-              ):(
-                <p>Shipping Address Form</p>
-              )
-            }
+            {activeItem === "paymentHistory" ? (
+              <PaymentHistory />
+            ) : (
+              this.renderAddresses()
+            )}
           </Grid.Column>
         </Grid.Row>
       </Grid>
-    )
+    );
   }
 }
 
-export default Profile;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
